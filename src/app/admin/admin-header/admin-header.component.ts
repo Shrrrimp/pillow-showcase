21a9691357257955d368b22c1b4584775746127f
Shrrrimp <juli150885@gmail.com>
2020-12-11 13:24:12 +0300

    feat: add admin header component

---
+++ src/app/admin/admin-header/admin-header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth/services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-header',
+  templateUrl: './admin-header.component.html',
+  styleUrls: ['./admin-header.component.scss']
+})
+export class AdminHeaderComponent implements OnInit {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  logOut(): void {
+    this.authService.logOut();
+    this.router.navigate(['/login']);
+  }
+
+}
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminMainComponent } from './pages/admin-main/admin-main.component';
 import { AdminRoutingModule } from '../admin/admin-routing.module';
+import { AdminHeaderComponent } from './admin-header/admin-header.component';
 
 
 
 @NgModule({
-  declarations: [AdminMainComponent],
+  declarations: [AdminMainComponent, AdminHeaderComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
